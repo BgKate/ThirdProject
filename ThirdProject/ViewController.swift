f257374a6bfb94f69fdd972faccc91481929dbe0
Ekaterina Bogdanova <katarina.bog@icloud.com>
2023-03-26 13:24:51 +0700

    Initial Commit

---
+++ ThirdProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdProject
+//
+//  Created by Ekaterina Bogdanova on 26/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
